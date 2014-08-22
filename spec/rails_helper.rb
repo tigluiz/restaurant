aa618f029625ea97f5a1631ee3c49ad385320851
Tiago Almeida <tig.luiz.almeida@gmail.com>
2014-08-21 22:20:28 -0300

    Add database cleaner to rspec config file

--- spec/rails_helper.rb
+++ spec/rails_helper.rb
@@ -11,4 +11,19 @@ ActiveRecord::Migration.maintain_test_schema!
 RSpec.configure do |config|
   config.include Capybara::DSL
   config.infer_spec_type_from_file_location!
+  config.before(:suite) do
+    DatabaseCleaner.clean_with(:truncation)
+  end
+  config.before(:each) do
+    DatabaseCleaner.strategy = :transaction
+  end
+  config.before(:each, :js => true) do
+    DatabaseCleaner.strategy = :truncation
+  end
+  config.before(:each) do
+    DatabaseCleaner.start
+  end
+  config.after(:each) do
+    DatabaseCleaner.clean
+  end
 end
