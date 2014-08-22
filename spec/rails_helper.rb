f2196ca18369cc787b111866fa0e430ba208419f
Tiago Almeida <tig.luiz.almeida@gmail.com>
2014-08-21 21:03:13 -0300

    Running rspec generator

---
+++ spec/rails_helper.rb
@@ -0,0 +1,14 @@
+ENV["RAILS_ENV"] ||= 'test'
+require 'spec_helper'
+require File.expand_path("../../config/environment", __FILE__)
+require 'rspec/rails'
+require 'capybara/rails'
+require 'capybara'
+
+Dir[Rails.root.join("spec/support/**/*.rb")].each { |f| require f }
+ActiveRecord::Migration.maintain_test_schema!
+
+RSpec.configure do |config|
+  config.include Capybara::DSL
+  config.infer_spec_type_from_file_location!
+end
