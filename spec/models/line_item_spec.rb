c86208dba02bc519580f282e828b74f82ffd9635
Tiago Almeida <tig.luiz.almeida@gmail.com>
2014-08-24 21:45:19 -0300

    Add relationship to orders

--- spec/models/line_item_spec.rb
+++ spec/models/line_item_spec.rb
@@ -1,6 +1,7 @@
 require 'rails_helper'
 
 RSpec.describe LineItem, :type => :model do
+  it { should belong_to(:order) }
   it { should validate_presence_of(:food_type) }
   it { should validate_presence_of(:quantity) }
   it { should validate_presence_of(:name) }
