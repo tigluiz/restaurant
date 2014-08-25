9d482c341d9611bf60e49ab520e2d1d011c4d930
Tiago Almeida <tig.luiz.almeida@gmail.com>
2014-08-24 21:41:19 -0300

    add order trait with items

--- spec/factories/order.rb
+++ spec/factories/order.rb
@@ -2,5 +2,10 @@ FactoryGirl.define do
   factory :order do
     state "created"
     table "42"
+    trait :with_items do
+      after(:create) do |instance|
+        create_list :item, 2, orders: [instance]
+      end
+    end
   end
 end
