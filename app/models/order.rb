62c6bf7d302b9c67555eaf10bdf1d979f4c97df6
Tiago Almeida <tig.luiz.almeida@gmail.com>
2014-08-24 21:39:53 -0300

    remove old relationship

--- app/models/order.rb
+++ app/models/order.rb
@@ -3,9 +3,7 @@ class Order < ActiveRecord::Base
   CONSTANT_FACTOR = 17
   after_create :initialize_order
   validates :table, presence: true
-
   has_many :line_items
-  has_and_belongs_to_many :sandwiches
 
   def initialize_order
     update_attributes(:number => (id * CONSTANT_FACTOR) + CONSTANT_NUMBER)
