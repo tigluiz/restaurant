833d3d348ac656081ed9311cf3ab575d6d3709aa
Tiago Almeida <tig.luiz.almeida@gmail.com>
2014-08-24 21:56:33 -0300

    Add real tests for order controller

--- spec/controllers/order_controller_spec.rb
+++ spec/controllers/order_controller_spec.rb
@@ -1,12 +1,21 @@
 require 'rails_helper'
 
 RSpec.describe OrderController, :type => :controller do
+  let(:order) {FactoryGirl.create(:order)}
 
   describe "GET new" do
     it "returns http success" do
-      get :new
+      get :show, {id: order.id}
       expect(response).to be_success
     end
   end
 
+  describe "POST create" do
+    let(:cart) {FactoryGirl.create(:cart, :with_items)}
+    it "create order" do
+      expect{
+        post :create, {cart_id: cart.id}
+      }.to change(Order, :count).by(1)
+    end
+  end
 end
