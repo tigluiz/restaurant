4753ef64ca60e8d17810e043186ef9c908ad65fb
Tiago Almeida <tig.luiz.almeida@gmail.com>
2014-08-24 21:40:40 -0300

    change order routes

--- config/routes.rb
+++ config/routes.rb
@@ -3,5 +3,7 @@ Rails.application.routes.draw do
   ActiveAdmin.routes(self)
   get 'pedido', to: 'order#new', as: 'orders'
   post 'pedido', to: "order#create", as: "orders_create"
+  get 'pedido/:id', to: 'order#show', as: 'order'
+  post 'pedido/:cart_id', to: "order#create", as: "orders_create"
   root 'home#index'
 end
