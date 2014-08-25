a58d861d0bc5a6631331b6eb6060cce8df502dca
Tiago Almeida <tig.luiz.almeida@gmail.com>
2014-08-24 21:40:58 -0300

    remove order old path

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,6 @@
 Rails.application.routes.draw do
   devise_for :admin_users, ActiveAdmin::Devise.config
   ActiveAdmin.routes(self)
-  get 'pedido', to: 'order#new', as: 'orders'
-  post 'pedido', to: "order#create", as: "orders_create"
   get 'pedido/:id', to: 'order#show', as: 'order'
   post 'pedido/:cart_id', to: "order#create", as: "orders_create"
   root 'home#index'
