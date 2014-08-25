04860a95eb836a8995ee2c1a1a7f08f4e8db1643
Tiago Almeida <tig.luiz.almeida@gmail.com>
2014-08-24 21:49:35 -0300

    Remove old relationship

--- app/models/cart.rb
+++ app/models/cart.rb
@@ -1,6 +1,5 @@
 class Cart < ActiveRecord::Base
   has_many :cart_items
-  has_many :sandwiches
   accepts_nested_attributes_for :cart_items, reject_if: proc { |attributes| attributes['genre'].blank? }
 
   def total
